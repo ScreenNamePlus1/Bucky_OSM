5f75df129f6bfaf11a8010c26e250f55451dded4
ScreenNamePlus1 <159028862+ScreenNamePlus1@users.noreply.github.com>
2025-08-12 12:58:03 -0400

    Update lib main.dart

--- lib main.dart
+++ lib main.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'providers/app_state.dart';
 import 'screens/login.dart';
+import 'screens/customer_home.dart';
+import 'screens/driver_home.dart';
+import 'screens/request_form.dart';
+import 'screens/bid_form.dart';
+import 'screens/bid_selection.dart';
+import 'screens/tracking.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Local Delivery',
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: LoginScreen(),
+      title: 'Local Delivery App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => LoginScreen(),
+        '/customer': (_) => CustomerHome(),
+        '/driver': (_) => DriverHome(),
+        '/request': (_) => RequestForm(),
+        '/bid': (_) => BidForm(),
+        '/bids': (_) => BidSelection(),
+        '/tracking': (_) => TrackingScreen(),
+      },
     );
   }
 }
