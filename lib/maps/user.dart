8c847ae6df09ba0e67fdce061699c23dbf0313d4
David <159028862+ScreenNamePlus1@users.noreply.github.com>
2025-08-14 22:57:49 -0400

    Create user.dart
    moved from lib/models/user.dart

---
+++ lib/maps/user.dart
@@ -0,0 +1,32 @@
+// lib/maps/user.dart
+class OSMUser {
+  final String id;
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  OSMUser({
+    required this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory OSMUser.fromJson(Map<String, dynamic> json) {
+    return OSMUser(
+      id: json['id'],
+      name: json['name'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+}
