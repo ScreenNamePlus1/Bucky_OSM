ce6fff7e1fcd1abb6a4a1ec5604306e3b01720cb
David <159028862+ScreenNamePlus1@users.noreply.github.com>
2025-08-16 10:09:52 -0400

    Update user.dart
    created based on usage

--- lib/maps/user.dart
+++ lib/maps/user.dart
@@ -1,32 +1,16 @@
-// lib/maps/user.dart
-class OSMUser {
+class AppUser {
   final String id;
-  final String name;
-  final double latitude;
-  final double longitude;
+  final String role;
 
-  OSMUser({
-    required this.id,
-    required this.name,
-    required this.latitude,
-    required this.longitude,
-  });
+  AppUser({required this.id, required this.role});
 
-  factory OSMUser.fromJson(Map<String, dynamic> json) {
-    return OSMUser(
-      id: json['id'],
-      name: json['name'],
-      latitude: json['latitude'],
-      longitude: json['longitude'],
-    );
-  }
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'role': role,
+      };
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'latitude': latitude,
-      'longitude': longitude,
-    };
-  }
+  factory AppUser.fromMap(Map<String, dynamic> map) => AppUser(
+        id: map['id'] ?? '',
+        role: map['role'] ?? 'customer',
+      );
 }
